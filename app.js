32c9a5c33c5f564c58eebf5281d0f2c1cddc6371
Anton Belko <30528165+ko1p@users.noreply.github.com>
2020-08-22 20:18:00 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,34 @@
-require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const rateLimit = require('express-rate-limit');
-const { errors } = require('celebrate');
+const { celebrate, Joi, errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const errorHandler = require('./middlewares/errorHandler');
 
-const {
-  PORT, DATABASE_URL, DATABASE_OPTIONS, RATE_LIMIT_CONFIG,
-} = require('./config');
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+const { PORT, DATABASE_URL, DATABASE_OPTIONS } = require('./config');
+
+const { createUser, login } = require('./controllers/users');
 
 const app = express();
 
-app.use(rateLimit(RATE_LIMIT_CONFIG));
+app.use(limiter);
 app.use(helmet());
+const corsOptions = {
+  origin: [
+    'http://localhost:8080',
+    'https://newsapp.ga',
+    'http://newsapp.ga',
+  ],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -25,13 +37,44 @@ mongoose.connect(DATABASE_URL, DATABASE_OPTIONS);
 
 app.use(requestLogger);
 
-app.use('/', require('./routes/index'));
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    about: Joi.string().required().min(2).max(30),
+    avatar: Joi.string().required(),
+  }),
+}), createUser);
+app.use('/cards', require('./routes/cards'));
+app.use('/users', require('./routes/users'));
+
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.use(errorLogger);
 
 app.use(errors());
 
-app.use(errorHandler);
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = statusCode === 500 ? 'Произошла ошибка' : err.message;
+  res.status(statusCode).send({ message });
+  next();
+});
 
 app.listen(PORT, () => {
   console.log(`Сервер запущен на ${PORT} порту`);
